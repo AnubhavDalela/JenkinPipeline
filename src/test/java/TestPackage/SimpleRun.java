0f398f197ba1a7d320e0d8825602cf876ddae3ab
SG0305808 <anubhav.dalela.ctr@sabre.com>
2020-03-23 02:12:22 +0530

    Myproject

---
+++ src/test/java/TestPackage/SimpleRun.java
@@ -0,0 +1,30 @@
+package TestPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class SimpleRun {
+
+    WebDriver driver = null;
+
+    @BeforeTest
+    public void initializer()
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\Drivers\\Selenium\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void Startup() throws IOException {
+        driver.get("https://www.google.com/");
+
+    }
+
+}
